e1e3f75c94e3d8992845b7cf6e35366d7969b17e
SnowWoofer <berealordietrying1@gmail.com>
2026-05-13 20:48:16 +0200

    slowly but surely, tomrrow: fully implement whisper, dsfsi and wav2vec2.0 asr for transcription comparison

--- src/asr.py
+++ src/asr.py
@@ -1,5 +1,10 @@
+import whisper
+
 def transcribe_whisper(audio_path, model_size="large-v3"):
-    import whisper
     model = whisper.load_model(model_size)
     result = model.transcribe(audio_path, language="tn")
+    print("YO")
     return result["text"]
+
+if __name__ == "__main__":
+    transcribe_whisper('data/raw_audio/audio')
